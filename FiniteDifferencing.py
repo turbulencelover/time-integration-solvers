2f8bfc64c3ed9e8f47f41ccc140e4a92a4a94ec4
Sneha Sinha <ssinha1.314@gmail.com>
2021-01-05 13:02:44 +0000

    finite differencing heat_1d

--- FiniteDifferencing.py
+++ FiniteDifferencing.py
@@ -1,54 +1,48 @@
 import numpy as np
 import time
 
-class FiniteDifferencing:
-    def dirchlet_central(self, alpha, f, boundary_conds, a, b, h):
-        n_increments = int((b-a)/h)
-        
+
+class Heat_fd:
+    def heat1d_fe(self, mu, f, boundary_conds, X, T, N, J):
+        a, b = X
+        dt = (T[1]-T[0])/N
+        dx = (b-a)/J
+
         # u(t,x)
-        u = np.zeros([n_increments+1, n_increments+1])
-        xs = np.linspace(a,b, n_increments+1)
+        u = np.zeros([N+1, J+1])
+        xs = np.arange(a, b + dx, dx)
 
         # initial condition u(t=0,x)=f(x)
         u[0] = [f(x) for x in xs]
+        
+        u[:,0], u[:,-1] = boundary_conds
+        
+        l = mu*(dt/dx**2)
 
-        for t in range(n_increments):
-            u[t+1] = [boundary_conds[0]] \
-                        + [u[t,n] + alpha**2*(h/(u[t,n]-u[t,n-1]))
-                            *(u[t,n+1]-2*u[t,n]+ u[t,n-1]) for n in range(1, n_increments)]\
-                        + [boundary_conds[1]]
-        return u
-
-    def neumann_central(self, alpha, f, boundary_conds, a, b, h):
-        n_increments = int((b-a)/h)
-        
-        # u(t,x)
-        u = np.zeros([n_increments+1, n_increments+1])
-        xs = np.linspace(a,b, n_increments+1)
-        
-        # initial condition u(t=0,x)=f(x)
-        u[0] = [f(x) for x in xs]
-        
-        for t in range(n_increments):
-            u[t+1]=[boundary_conds]+[u[t,n] + 2*(alpha**2)*(h/(u[t,n]-u[t,n-1]))*(u[t,n-1]-u[t,n]) for n in range(1,n_increments+1)]
+        for t in range(N):
+            u[t+1, 1:J] = [u[t, n] + l*(u[t, n+1]-2*u[t, n] + u[t, n-1])
+                          for n in range(1, J)]
 
         return u
+
 
 if __name__ == '__main__':
     # neuman (only when du_dx=0)
 
-    ics = lambda x : x*(1-x)
-    alpha =1
-    bcs_d= [0,0]
-    bcs_n = 0
-    a=0
-    b=1
-    h=0.1
-    u = FiniteDifferencing().dirchlet_central(#.neumann_central(
-            alpha=alpha,
-            f=ics,
-            boundary_conds=bcs_d,
-            a=a,
-            b=b, 
-            h=h)
+    def ics(x): return x*(1-x)
+    mu = 1
+    bcs = (0,0)
+    X= (0,1)
+    J = 10
+    N = 10
+    T = (0,5)
+    u = Heat_fd().heat1d_fe(
+        mu=mu,
+        f=ics,
+        boundary_conds=bcs,
+        X=X,
+        T=T,
+        N=N,
+        J=J)
+
     print(u)
